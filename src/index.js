a4bbc3d597b14035e0b47aa7fb140c0dab2d9b45
Georgiy Garenov <g_garenov@outlook.com>
2023-12-11 15:39:08 +0200

    Basic installation. - Express JS - Express-handlebars - bcrypt - cookie-parser - jsonwebtoken - mongoose - nodemon
    Static files were installed like views and public

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+//Express Config
+
+app.use(express(path.resolve(__dirname, "./public")));
+
+//Handlebars Config
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+app.set("view engine", hbs);
+app.set("views", "src/views");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
