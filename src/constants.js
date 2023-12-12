ef4b7c94649b72ea32288aca6b6c2f1ddfe46bab
Georgiy Garenov <g_garenov@outlook.com>
2023-12-12 11:37:50 +0200

    jsonwebtoken is implemented in userController, userService, constants.js and jwt.js is created also we test it with console.log so that we receive the token :)

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 const CONFIG = {
   PORT: 3000,
   DB_URL: "mongodb://127.0.0.1:27017/regular_exam", //localhost = 127.0.0.1
+  SECRET: "4161f938-1d97-4a75-bd34-079be7ed970b",
 };
 
 module.exports = CONFIG;
