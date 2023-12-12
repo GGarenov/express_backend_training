46592fca9917849d9f0b5c80ccb7e3b0a15cd510
Georgiy Garenov <g_garenov@outlook.com>
2023-12-12 13:27:58 +0200

    Samo az si znam kvo si znam, kogato si znam

---
+++ userControllerOLD.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const userService = require("../services/userService");
+const { extractErrorMsgs } = require("../utils/errorHandler");
+
+router.get("/register", (req, res) => {
+  res.render("users/register");
+});
+
+router.post("/register", async (req, res) => {
+  const { email, username, password, repassword } = req.body;
+
+  try {
+    await userService.register({ email, username, password, repassword });
+
+    res.redirect("/users/login");
+  } catch (error) {
+    const errorMessages = extractErrorMsgs(error);
+    res.status(404).render("users/register", { errorMessages });
+  }
+});
+
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
+
+router.post("/login", async (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+
+  try {
+    const token = await userService.login(email, password);
+
+    res.cookie("token", token, { httpOnly: true });
+    res.redirect("/");
+  } catch (error) {
+    const errorMessages = extractErrorMsgs(error);
+    res.status(404).render("users/login", { errorMessages });
+  }
+});
+
+router.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.redirect("/");
+});
+
+module.exports = router;
