bbc4fc835509b699f91ce5eacef8b6c66b3aea95
Georgiy Garenov <g_garenov@outlook.com>
2023-12-12 12:56:02 +0200

    Min requirements for user to register

--- src/models/User.js
+++ src/models/User.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true },
-  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true, minLength: 10 },
+  username: { type: String, required: true, minLength: 3 },
+  password: { type: String, required: true, minLength: 4 },
 });
 
 userSchema.virtual("repassword").set(function (value) {
